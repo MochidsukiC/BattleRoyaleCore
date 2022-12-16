d536ccd1e7f13aeff867bc377bf3ab1f5c820206
BuildTools <unconfigured@null.spigotmc.org>
2022-12-16 14:07:55 +0900

    Initial commit

---
+++ src/main/java/net/mochidsuki/corsica/battleroyalecore/MiniMapRenderer.java
@@ -0,0 +1,14 @@
+package net.mochidsuki.corsica.battleroyalecore;
+
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+import org.jetbrains.annotations.NotNull;
+
+public class MiniMapRenderer extends MapRenderer {
+    @Override
+    public void render(@NotNull MapView map, @NotNull MapCanvas canvas, @NotNull Player player) {
+
+    }
+}
--- src/main/java/net/mochidsuki/corsica/battleroyalecore/event.java
+++ src/main/java/net/mochidsuki/corsica/battleroyalecore/event.java
@@ -2,6 +2,7 @@ package net.mochidsuki.corsica.battleroyalecore;
 
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -9,6 +10,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.map.MapView;
 import org.bukkit.scoreboard.Team;
 
 import static org.bukkit.event.entity.EntityDamageEvent.DamageCause.SUFFOCATION;
@@ -18,18 +22,30 @@ public class event implements Listener{
     public void PlayerDropItemEvent(PlayerDropItemEvent event){
         Item item = event.getItemDrop();
         if(item.getItemStack().getType() == Material.FIREWORK_ROCKET){//ドロップシップからの降下
+            Player player = event.getPlayer();
             try {
-                Team playerteam = event.getPlayer().getScoreboard().getPlayerTeam(event.getPlayer());
+                Team playerteam = player.getScoreboard().getPlayerTeam(player);
                 String[] tp = new String[playerteam.getEntries().size()];
                 playerteam.getEntries().toArray(tp);
                 Player[] teamplayer = new Player[tp.length];
                 for (int i = 0;i < tp.length;i++) {
                     teamplayer[i] = Bukkit.getPlayer(tp[i]);
                 }
-                for (int i = 0; i < teamplayer.length; i++){
+                for (int i = 0; i < teamplayer.length; i++){//teamplayer全員に実行
                     teamplayer[i].addScoreboardTag("live");
+                    teamplayer[i].setGameMode(GameMode.SURVIVAL);
                 }
+                ItemStack mapItem = new ItemStack(Material.FILLED_MAP,1);
+                MapMeta mapMeta = (MapMeta)mapItem.getItemMeta();
+
+                MapView view = player.getServer().createMap(player.getWorld());
+
+                view.addRenderer(new);
+                mapMeta.setMapView(view);
+                mapItem.setItemMeta(mapMeta);
+
             }catch (Exception e){}
+
         }
     }
     @EventHandler

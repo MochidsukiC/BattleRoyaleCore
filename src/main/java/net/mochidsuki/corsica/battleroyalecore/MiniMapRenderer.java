e9b54c658bb84a436a811a6a3ee4933495f00145
BuildTools <unconfigured@null.spigotmc.org>
2022-12-16 16:12:14 +0900

    Initial commit

--- src/main/java/net/mochidsuki/corsica/battleroyalecore/MiniMapRenderer.java
+++ src/main/java/net/mochidsuki/corsica/battleroyalecore/MiniMapRenderer.java
@@ -9,7 +9,8 @@ import org.jetbrains.annotations.NotNull;
 public class MiniMapRenderer extends MapRenderer {
     @Override
     public void render(@NotNull MapView map, @NotNull MapCanvas canvas, @NotNull Player player) {
-
+        map.setCenterX(player.getLocation().getBlockX());
+        map.setCenterZ(player.getLocation().getBlockZ());
 
     }
 }

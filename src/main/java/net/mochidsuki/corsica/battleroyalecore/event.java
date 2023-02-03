6705b1af6182e93e7ac5d753beb662f596cf367d
BuildTools <unconfigured@null.spigotmc.org>
2023-02-04 07:31:44 +0900

    from Mochidsuki_High

--- src/main/java/net/mochidsuki/corsica/battleroyalecore/event.java
+++ src/main/java/net/mochidsuki/corsica/battleroyalecore/event.java
@@ -1,12 +1,11 @@
 package net.mochidsuki.corsica.battleroyalecore;
 
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Fireball;
-import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
+import org.bukkit.Sound;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -15,10 +14,15 @@ import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.Team;
 
+
+import java.util.Objects;
 
 import static org.bukkit.event.entity.EntityDamageEvent.DamageCause.SUFFOCATION;
 
@@ -28,16 +32,16 @@ public class event implements Listener{
         Item item = event.getItemDrop();
         if(item.getItemStack().getType() == Material.FIREWORK_ROCKET){//ドロップシップからの降下
             Team playerteam = event.getPlayer().getScoreboard().getPlayerTeam(event.getPlayer());
-            String[] tp = new String[playerteam.getEntries().size()];
+            String[] tp = new String[Objects.requireNonNull(playerteam).getEntries().size()];
             playerteam.getEntries().toArray(tp);
             Player[] teamplayer = new Player[tp.length];
             for (int i = 0;i < tp.length;i++) {
                 teamplayer[i] = Bukkit.getPlayer(tp[i]);
             }
 
-            for (int i = 0; i < teamplayer.length; i++) {//teamplayer全員に実行
+            for (Player player : teamplayer) {//teamplayer全員に実行
                 GameStart g = new GameStart();
-                g.player(teamplayer[i]);
+                g.player(player);
             }
 
 
@@ -79,7 +83,7 @@ public class event implements Listener{
     @EventHandler
     public void PlayerInteractEvent(PlayerInteractEvent event){
         if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK){
-            if(event.getItem().getType() == Material.FIRE_CHARGE){
+            if(Objects.requireNonNull(event.getItem()).getType() == Material.FIRE_CHARGE){
                 Fireball fireball = event.getPlayer().getWorld().spawn(event.getPlayer().getEyeLocation(), Fireball.class);
                 fireball.setShooter(event.getPlayer());
                 fireball.setVelocity(event.getPlayer().getLocation().getDirection().normalize().multiply(1.5));
@@ -90,9 +94,62 @@ public class event implements Listener{
 
     @EventHandler
     public void EntityDamageByEntityEvent(EntityDamageByEntityEvent event){
-        if(event.getEntityType() == EntityType.PLAYER) {
-            Player player = (Player) event.getDamager();
-            player.setLevel((int) event.getDamage());
+        Player damager = null;
+        Player player = (Player) event.getEntity();
+        boolean damagerType = true;
+        try {
+            damager = (Player) event.getDamager();
+        }catch (Exception e){
+            damagerType = false;
+        }
+
+        if(event.getEntity().getType().equals(EntityType.PLAYER)) {
+            if(damagerType) {
+                damager.setLevel((int) event.getDamage());//ダメージを経験値に変換
+            }
+
+
+            //シールド
+            int shieldMax;
+            int shieldNow;
+            int damage = 0;
+            ItemStack chestItem = player.getInventory().getItem(EquipmentSlot.CHEST);
+            switch (Objects.requireNonNull(chestItem).getType()){
+                case CHAINMAIL_CHESTPLATE:
+                    shieldMax = 5;
+                    break;
+                case IRON_CHESTPLATE:
+                case GOLDEN_CHESTPLATE:
+                    shieldMax = 10;
+                    break;
+                case DIAMOND_CHESTPLATE:
+                    shieldMax = 15;
+                    break;
+                case NETHERITE_CHESTPLATE:
+                    shieldMax = 20;
+                    break;
+                default:
+                    shieldMax = 0;
+            }
+            Damageable d = (Damageable)chestItem.getItemMeta();
+            shieldNow = (chestItem.getType().getMaxDurability()-Objects.requireNonNull(d).getDamage())/chestItem.getType().getMaxDurability()*shieldMax;
+            if(shieldNow>0){
+                damage = (int) (event.getDamage() - shieldNow);
+                shieldNow = (int) (shieldNow - event.getDamage());
+                if(shieldNow <= 0){
+                    shieldNow = 0;
+                    if(damagerType) {
+                        damager.playSound(damager.getLocation(), Sound.BLOCK_GLASS_BREAK, 100, 0);
+                    }
+                }
+            d.setDamage(shieldNow/shieldMax*chestItem.getType().getMaxDurability());
+            chestItem.setItemMeta(d);
+            }
+            if(damage <= 0){
+                event.setCancelled(true);
+            }else {
+                event.setDamage(damage);
+            }
         }
     }
 }
--- src/main/java/net/mochidsuki/corsica/battleroyalecore/everyticks.java
+++ src/main/java/net/mochidsuki/corsica/battleroyalecore/everyticks.java
@@ -1,15 +1,21 @@
 package net.mochidsuki.corsica.battleroyalecore;
 
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
-import java.awt.*;
+import java.util.Collections;
+import java.util.Objects;
 
 public class everyticks extends BukkitRunnable {
     int s = 19;
@@ -29,7 +35,51 @@ public class everyticks extends BukkitRunnable {
 
         //everyticks
         Player[] players = Bukkit.getServer().getOnlinePlayers().toArray((new Player[0]));
-        for (Player player : players) {
+        for (Player player : players) {//全プレイヤーに適応
+            //タイトル
+            int shieldMax;
+            ItemStack chestItem = player.getInventory().getItem(EquipmentSlot.CHEST);
+            switch (Objects.requireNonNull(chestItem).getType()){
+                case CHAINMAIL_CHESTPLATE:
+                    shieldMax = 5;
+                    break;
+                case IRON_CHESTPLATE:
+                case GOLDEN_CHESTPLATE:
+                    shieldMax = 10;
+                    break;
+                case DIAMOND_CHESTPLATE:
+                    shieldMax = 15;
+                    break;
+                case NETHERITE_CHESTPLATE:
+                    shieldMax = 20;
+                    break;
+                default:
+                    shieldMax = 0;
+            }
+
+
+            TextComponent component = new net.md_5.bungee.api.chat.TextComponent();
+            String shield;
+            String half;
+            String openings;
+            Damageable d = (Damageable)chestItem.getItemMeta();
+            shield =  String.join("", Collections.nCopies(((int)((chestItem.getType().getMaxDurability()-Objects.requireNonNull(d).getDamage())/chestItem.getType().getMaxDurability()*shieldMax))/2, "■"));
+            if(!((Objects.requireNonNull(d).getDamage()/chestItem.getType().getMaxDurability()*shieldMax)%2 == 0)){
+                half = "□";
+            }else {
+                half = "";
+            }
+            openings =  String.join("", Collections.nCopies((shieldMax-(int)((chestItem.getType().getMaxDurability()-Objects.requireNonNull(d).getDamage())/chestItem.getType().getMaxDurability()*shieldMax))/2, "-"));
+            component.setText("["+shield+half+openings+"]");
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR,component);
+
+
+
+
+
+
+
+            //エリトラのエフェクト
             if (player.hasPotionEffect(PotionEffectType.SLOW_FALLING)) {
                 Location l = player.getLocation();
                 l.setY(player.getLocation().getBlockY() + 1);

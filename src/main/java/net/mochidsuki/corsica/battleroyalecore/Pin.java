8db25256429264fc8ea00dcc0617f9235babe048
BuildTools <unconfigured@null.spigotmc.org>
2023-02-05 11:45:31 +0900

    from Mochidsuki_High

--- src/main/java/net/mochidsuki/corsica/battleroyalecore/Pin.java
+++ src/main/java/net/mochidsuki/corsica/battleroyalecore/Pin.java
@@ -4,6 +4,7 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -15,12 +16,14 @@ public class Pin {
         try {
             ProtocolManager manager = ProtocolLibrary.getProtocolManager();
             PacketContainer slime_spawn = manager.createPacket(PacketType.Play.Server.SPAWN_ENTITY_LIVING);
-            slime_spawn.getIntegers().write(0, slime_id).write(1, (int) EntityType.SLIME.getTypeId()); //doesn't spawn a slime
+            slime_spawn.getEntityTypeModifier().write(0, EntityType.SLIME);
             slime_spawn.getUUIDs().write(0, UUID.randomUUID());
             slime_spawn.getDoubles().write(0, loc.getX()).write(1, loc.getY()).write(2, loc.getZ());
-            slime_id--;
 
             PacketContainer slime_meta = manager.createPacket(PacketType.Play.Server.ENTITY_METADATA);
+
+            WrappedDataWatcher watcher = new WrappedDataWatcher(); //Create data watcher, the Entity Metadata packet requires this
+            WrappedDataWatcher.Serializer serializer = WrappedDataWatcher.Registry.get(Byte.class); //Found this through google, needed for some stupid reason
 //what to put here?
 
             manager.sendServerPacket(player, slime_spawn);

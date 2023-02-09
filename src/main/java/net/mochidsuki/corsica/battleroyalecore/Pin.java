819a4e9630a5db7d4ef3916033be747d217be3de
BuildTools <unconfigured@null.spigotmc.org>
2023-02-09 21:09:32 +0900

    from Mochidsuki_High

--- src/main/java/net/mochidsuki/corsica/battleroyalecore/Pin.java
+++ src/main/java/net/mochidsuki/corsica/battleroyalecore/Pin.java
@@ -1,6 +1,7 @@
 package net.mochidsuki.corsica.battleroyalecore;
 
 import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLib;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
@@ -12,41 +13,29 @@ import org.bukkit.entity.Player;
 import java.lang.reflect.InvocationTargetException;
 import java.util.UUID;
 
-public class Pin {
+public class Pin extends ProtocolLib {
     public void pin(Player player, Location loc) {
-        /*
-        try {
-            ProtocolManager manager = ProtocolLibrary.getProtocolManager();
-            PacketContainer slime_spawn = manager.createPacket(PacketType.Play.Server.SPAWN_ENTITY_LIVING);
-            slime_spawn.getEntityTypeModifier().write(0, EntityType.SLIME);
-            slime_spawn.getUUIDs().write(0, UUID.randomUUID());
-            slime_spawn.getDoubles().write(0, loc.getX()).write(1, loc.getY()).write(2, loc.getZ());
-
-
-            WrappedDataWatcher watcher = new WrappedDataWatcher(); //Create data watcher, the Entity Metadata packet requires this
-            WrappedDataWatcher.Serializer serializer = WrappedDataWatcher.Registry.get(Byte.class); //Found this through google, needed for some stupid reason
-            watcher.setEntity(player);
-            watcher.setObject(0, serializer, (byte) (0x40));
-//what to put here?
-            slime_spawn.getWatchableCollectionModifier().write(0, watcher.getWatchableObjects()); //Make the packet's datawatcher the one we created
-            try {
-                manager.sendServerPacket(player, slime_spawn);
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-            manager.sendServerPacket(player, slime_spawn);
-        }catch (Exception e){
-
-        }
-         */
         ProtocolManager pm = ProtocolLibrary.getProtocolManager();
-        PacketContainer packet = pm.createPacket(PacketType.Play.Server.ENTITY_METADATA);
-        packet.getIntegers().write(0, player.getEntityId()); //Set packet's entity id
-        WrappedDataWatcher watcher = new WrappedDataWatcher(); //Create data watcher, the Entity Metadata packet requires this
-        WrappedDataWatcher.Serializer serializer = WrappedDataWatcher.Registry.get(Byte.class); //Found this through google, needed for some stupid reason
-        watcher.setEntity(player); //Set the new data watcher's target
-        watcher.setObject(0, serializer, (byte) (0x40)); //Set status to glowing, found on protocol page
-        packet.getWatchableCollectionModifier().write(0, watcher.getWatchableObjects()); //Make the packet's datawatcher the one we created
+        PacketContainer packet = pm.createPacket(PacketType.Play.Server.SPAWN_ENTITY);
+        packet.getModifier().writeDefaults();
+        packet.getEntityTypeModifier().write(0, EntityType.SLIME);
+        packet.getUUIDs().write(0, UUID.randomUUID());
+        packet.getIntegers().write(1, 1); //set the type of the entity,probably the problem?
+        packet.getDoubles().write(0, loc.getX());
+        packet.getDoubles().write(1, loc.getY());
+        packet.getDoubles().write(2, loc.getZ());
+
+        WrappedDataWatcher.Serializer byteSerializer = WrappedDataWatcher.Registry.get(Byte.class); //Serializer for data watcher object's value
+        WrappedDataWatcher.WrappedDataWatcherObject entityBitmask = new WrappedDataWatcher.WrappedDataWatcherObject(0, byteSerializer); //Creating a data watcher object with it's index and serializer for expected value type
+
+        WrappedDataWatcher watcher = new WrappedDataWatcher();
+        watcher.setObject(entityBitmask, (byte) 0x40); //Enable glow effect
+
+        WrapperPlayServerEntityMetadata metadataPacket = new WrapperPlayServerEntityMetadata();
+        metadataPacket.setEntityID(packet.getId());
+        metadataPacket.setMetadata(watcher.getWatchableObjects());
+        metadataPacket.sendPacket(player);
+
         try {
             pm.sendServerPacket(player, packet);
         } catch (InvocationTargetException e) {
